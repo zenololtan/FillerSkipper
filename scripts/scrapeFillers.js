26fcc57e44802fb3bfbd4b563db708f0a84066cc
Ingmar Kole <ikole@student.codam.nl>
2021-03-21 12:16:18 +0100

    this sorta works i guess

---
+++ scripts/scrapeFillers.js
@@ -0,0 +1,23 @@
+// when ran on animefillerlist.com it will parse the filler episodes to json
+
+var collection = document.getElementsByClassName('filler');
+var i;
+var fillers = [];
+var show;
+
+// get name of show
+show = window.location.pathname.split("/")[2].replace("-", " ");
+console.log(show);
+
+// make colletion of all the episodes
+for (i = 1; collection[i]; i++) {
+	var item = collection[i].getElementsByClassName('Number');
+	fillers.push(item.item(0).innerHTML);
+}
+
+//parse the collection to json format
+var obj = fillers.reduce((map, filler) => { 
+    map.fillers.push( { episode: filler} );
+    return map;
+}, { fillers: []})
+console.log(JSON.stringify(obj, null, 4));
