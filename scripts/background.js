e12d071920af2c4b2a00621145190bafd988a18b
Ingmar Kole <ikole@student.codam.nl>
2021-03-21 12:47:21 +0100

    now injects on pageload

--- scripts/background.js
+++ scripts/background.js
@@ -1,8 +1,11 @@
 // console.log('im here!');
 chrome.tabs.onActivated.addListener(tab => {
 	chrome.tabs.get(tab.tabId, current_tab_info => {
-		if (/^https:\/\/4anime.to/.test(current_tab_info.url)) {
-			chrome.tabs.executeScript(null, {file: './scripts/foreground.js'}, () => console.log('Foreground script injected'))
-		}
+		chrome.tabs.onUpdated.addListener(info => {
+			if (/^https:\/\/4anime.to/.test(current_tab_info.url)) {
+				chrome.tabs.executeScript(null, {file: './scripts/foreground.js'}, () => console.log('Foreground script injected'))
+			}
+		});
 	});
 });
+
